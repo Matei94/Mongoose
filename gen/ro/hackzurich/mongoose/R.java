a009377a5c4becdaf772c56288def18a966eba9f
Matei94 <matei.rm94@gmail.com>
2014-10-11 06:41:31 +0200

    paypal working

--- gen/ro/hackzurich/mongoose/R.java
+++ gen/ro/hackzurich/mongoose/R.java
@@ -1099,12 +1099,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btnLogin=0x7f050043;
-        public static final int btnScore=0x7f050046;
+        public static final int btnPaypal=0x7f050044;
+        public static final int btnScore=0x7f050047;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003d;
@@ -1155,7 +1156,7 @@ containing a value of this type.
         public static final int txtvwLogout=0x7f050040;
         public static final int txtvwNotifications=0x7f050041;
         public static final int txtvwPendings=0x7f050042;
-        public static final int txtvwRanking=0x7f050044;
+        public static final int txtvwRanking=0x7f050045;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
--- src/ro/hackzurich/mongoose/Login.java
+++ src/ro/hackzurich/mongoose/Login.java
@@ -1,14 +1,30 @@
 package ro.hackzurich.mongoose;
 
+import java.math.BigDecimal;
+
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
 public class Login extends Activity {
 	private Button btnLogin;
+	private Button btnPaypal;
+	
+	private static PayPalConfiguration config = new PayPalConfiguration()
+    .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
+    .clientId("AYNZXBB9-C5TvuB_dbNlRfitgvL_3aXeJ_Gn8rN9JbqRNS-9XWUOkm45wP7S");
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +40,52 @@ public class Login extends Activity {
 				startActivity(goToMainActivityIntent);
 			}
 		});
+		
+		btnPaypal = (Button) findViewById(R.id.btnPaypal);
+		btnPaypal.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				 	PayPalPayment payment = new PayPalPayment(new BigDecimal("1.75"), "USD", "hipster jeans",
+				 			PayPalPayment.PAYMENT_INTENT_SALE);
+				    Intent intent = new Intent(Login.this, PaymentActivity.class);
+				    intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+				    startActivityForResult(intent, 0);
+			}
+		});
+		
+		Intent intent = new Intent(this, PayPalService.class);
+	    intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+	    startService(intent);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		stopService(new Intent(this, PayPalService.class));
+		super.onDestroy();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+	        PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+	        if (confirm != null) {
+	            try {
+	                Log.i("MONGOOSE", confirm.toJSONObject().toString(4));
+
+	                // TODO: send 'confirm' to your server for verification.
+	                // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+	                // for more details.
+
+	            } catch (JSONException e) {
+	                Log.e("MONGOOSE", "an extremely unlikely failure occurred: ", e);
+	            }
+	        }
+	    }
+	    else if (resultCode == Activity.RESULT_CANCELED) {
+	        Log.i("MONGOOSE", "The user canceled.");
+	    }
+	    else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+	        Log.i("MONGOOSE", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+	    }
 	}
 }
