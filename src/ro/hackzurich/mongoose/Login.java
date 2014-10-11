40fa5ccd8ba709a85139fe12897457e735472313
Nicu Badescu <badescunicu@yahoo.com>
2014-10-11 11:06:22 +0200

    Add Facebook Login (v1)

--- src/ro/hackzurich/mongoose/Login.java
+++ src/ro/hackzurich/mongoose/Login.java
@@ -1,28 +1,35 @@
 package ro.hackzurich.mongoose;
 
-import android.app.Activity;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.widget.UserSettingsFragment;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
-public class Login extends Activity {
-	private Button btnLogin;
+public class Login extends FragmentActivity {
+	 private UserSettingsFragment userSettingsFragment;
+
+	 public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.login);
+         FragmentManager fragmentManager = getSupportFragmentManager();
+         userSettingsFragment = (UserSettingsFragment) fragmentManager.findFragmentById(R.id.login_fragment);
+         userSettingsFragment.setSessionStatusCallback(new Session.StatusCallback() {
+             @Override
+             public void call(Session session, SessionState state, Exception exception) {
+                 Log.d("LoginUsingLoginFragmentActivity", String.format("New session state: %s", state.toString()));
+             }
+         });
+	 }
+
+	 @Override
+	 public void onActivityResult(int requestCode, int resultCode, Intent data) {
+         userSettingsFragment.onActivityResult(requestCode, resultCode, data);
+         super.onActivityResult(requestCode, resultCode, data);
+	 }
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
-		
-		btnLogin = (Button) findViewById(R.id.btnLogin);
-		btnLogin.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent goToMainActivityIntent = new Intent(Login.this, 
-						MainActivity.class);
-				startActivity(goToMainActivityIntent);
-			}
-		});
-	}
 }
